20bf69ed625969d8b8674bd2fb3bc848d5c5efae
JayX97 <jason.fernandezx@yahoo.com>
2025-04-08 21:27:49 -0400

    Displayed items from JSON file to webpage using Fetch API

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-import data from "./data.json" assert { type: 'json' };
+const itemContainer = document.getElementById('items-container'); // container to append items
+
+let data;
+
+fetch("./data.json")
+    .then(response => response.json())
+    .then(data => data.forEach(item => { // takes JSON file data from fetch function call and displays data to webpage
+        const itemBox = document.createElement("div");
+        itemBox.className = "item";
+    
+        itemBox.innerText = item.name;
+        itemContainer.appendChild(itemBox);
+    }))
+    .catch(error => console.log(error));
