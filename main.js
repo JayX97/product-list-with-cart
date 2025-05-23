405fe377cc0d2142c7e18aaf2662423023acd798
JayX97 <jason.fernandezx@yahoo.com>
2025-05-23 19:49:36 -0400

    Starting to work on additional styling for add to cart button

--- main.js
+++ main.js
@@ -57,6 +57,8 @@ const printTotal = () => { // add up total from item quantities in cart
     totalPrintDiv.innerText = "$" + parseFloat(grandTotal).toFixed(2);
 }
 
+// ** ADD FUNCTIONS FOR ITEM INCREMENT/DECREMENT **
+
 // DISPLAY INDIVIDUAL ITEMS AVAILABLE FROM DATABASE
 
 fetch("./data.json") // fetch data from data.json using Fetch API
@@ -91,6 +93,27 @@ fetch("./data.json") // fetch data from data.json using Fetch API
         
         addButton.innerHTML = addButton.innerHTML + " Add to Cart";
         addButton.className = "add-button";
+
+        // ** DIV FOR ITEM QUANTITY ADJUSTMENT **
+        const itemQuantity = document.createElement("div");
+        itemQuantity.className = "adjust-quantity";
+
+        const decrement = document.createElement("div");
+        decrement.className = "decrement-button";
+        decrement.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="2" fill="none" viewBox="0 0 10 2"><path fill="#fff" d="M0 .375h10v1.25H0V.375Z"/></svg>';
+        itemQuantity.appendChild(decrement);
+
+        const amount = document.createElement("p");
+        amount.className = "item-current-quantity";
+        amount.innerText = item.quantity;
+        itemQuantity.appendChild(amount);
+
+        const increment = document.createElement("div");
+        increment.className = "increment-button";
+        increment.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#fff" d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"/></svg>';
+        itemQuantity.appendChild(increment);
+
+        addButton.appendChild(itemQuantity);
 
         // item category
         const itemCategory = document.createElement("p");
